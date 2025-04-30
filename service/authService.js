5094ba660f6cf72c9693c6b091dc0067ae508a05
Hagar wael <141748510+HagarWael@users.noreply.github.com>
2025-04-30 15:51:55 +0300

    fixes in auth

--- service/authService.js
+++ service/authService.js
@@ -5,48 +5,16 @@ const secret = process.env.JWT_SECRET;
 const Patient = require("../model/Patient");
 const Doctor = require("../model/Doctor");
 require("dotenv").config();
-const ExpiredToken = require("../model/expiredToken");
 
-const registeredUser = async ({
-  fullname,
-  email,
-  password,
-  role,
-  doctorCode,
-}) => {
+const registeredUser = async ({ fullname, email, password, role }) => {
   let user = await User.findOne({ email });
-  if (user) throw new Error("already exists");
+  if (user) throw new Error("User already exists");
 
   const hashedPass = await bcrypt.hash(password, 10);
-
-  if (role === "doctor") {
-    const generateUniqueCode = () => {
-      return "DR-" + Math.random().toString(36).substring(2, 8).toUpperCase();
-    };
-
-    const uniqueCode = generateUniqueCode();
-
-    user = new Doctor({
-      fullname,
-      email,
-      password: hashedPass,
-      role,
-      uniqueCode,
-    });
-  } else if (role === "patient") {
-    if (!doctorCode) throw new Error("doc code is required");
-
-    const doctor = await Doctor.findOne({ uniqueCode: doctorCode });
-    if (!doctor) throw new Error("doc not found with this code");
-
-    user = new Patient({
-      fullname,
-      email,
-      password: hashedPass,
-      role,
-      doctor: doctor._id,
-    });
+  if (role === "patient" || role === "doctor") {
+    user = new User({ fullname, email, password: hashedPass, role });
   } else {
+    console.log("invalid role:", role);
     throw new Error("Invalid role");
   }
 
